b4d1f73c3a11b2ae3c3ec88b211eb1e7a0478b04
TerKrubKrub <jirakan.cop@gmail.com>
2021-11-26 00:16:56 +0700

    .

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-print("Copter")
-print("KuyRaiAiSus")
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+class window(QWidget):
+    def __init__(self, parent = None):
+        super(window, self).__init__(parent)
+        self.resize(200, 50)
+        self.setWindowTitle("test Lib")
+        self.label = QLabel(self)
+        self.label.setText("1234")
+        font = QFont()
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.move(50, 20)
+
+def main():
+    app = QApplication(sys.argv)
+    ex = window()
+    ex.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
