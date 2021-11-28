94131fc9d1597411e9c5facbedebcc36cfd2a6b0
Chanon <pcg.mega@gmail.com>
2021-11-28 18:40:10 +0700

    authen:gui+db

--- src/gui/authen.py
+++ src/gui/authen.py
@@ -1,32 +1,33 @@
-import sys,os
+import sys, os
 import sqlite3
 from PyQt5 import QtWidgets, QtCore, QtGui, uic
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import rsrc.rsrc
+import rsrc.style.authen as style
 
 
 class LogIn(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("rsrc/ui/login.ui", self)
+        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
         self.fontDB = QtGui.QFontDatabase()
         self.fontDB.addApplicationFont(":/Font/font/Better Grade/Better Grade.ttf")
         self.fontDB.addApplicationFont(
             ":/Font/font/Product Sans/Product Sans Regular.ttf"
         )
-        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.db = sqlite3.connect("rsrc/db/data.db")
+        self.curs = self.db.cursor()
+        self.setStyleSheet(style.default)
         self.ext_btn.mousePressEvent = self.exit
         self.min_btn.mousePressEvent = self.minimize
         self.show_pwd.mousePressEvent = self.showPassword
         self.login_btn.mousePressEvent = self.logIn
-        self.signup_btn.mousePressEvent = self.signUp
+        self.create_btn.mousePressEvent = self.signUp
         self.password.textChanged.connect(self.hidePassword)
         self.cur_pos = QtCore.QPoint(1080, 620)
-        self.usnErrStyle = "#username { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.usnNormStyle = "#username { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
-        self.pwdErrStyle = "#password { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.pwdNormStyle = "#password { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
 
     def mousePressEvent(self, event):
         self.cur_pos = event.globalPos()
@@ -43,49 +44,74 @@ class LogIn(QtWidgets.QWidget):
 
     def usnChanged(self, txt):
         if not txt:
-            self.username.setStyleSheet(self.usnErrStyle)
+            self.username.setStyleSheet(style.error)
         else:
-            self.username.setStyleSheet(self.usnNormStyle)
+            self.username.setStyleSheet(style.input)
 
     def pwdChanged(self, txt):
         if not txt:
-            self.password.setStyleSheet(self.pwdErrStyle)
+            self.password.setStyleSheet(style.error)
         else:
-            self.password.setStyleSheet(self.pwdNormStyle)
+            self.password.setStyleSheet(style.input)
 
     def logIn(self, event):
-        if (
-            self.username.text() == "admin" and self.password.text() == "12345678"
-        ):  # if matched
-            print(self.username.text())
-            print(self.password.text())
+        self.curs.execute("SELECT username FROM users")
+        self.users_db = self.curs.fetchall()
+        self.usn_db = [i[0] for i in self.users_db]
+        if self.username.text() in self.usn_db:
+            self.curs.execute(
+                'SELECT password FROM users WHERE username="'
+                + self.username.text()
+                + '"'
+            )
+            self.pwd_db = self.curs.fetchone()[0]
+            if self.password.text() == self.pwd_db:
+                self.curs.execute(
+                    'SELECT user_id FROM users WHERE username="'
+                    + self.username.text()
+                    + '"'
+                )
+                self.user_id = self.curs.fetchone()[0]
+                print("\nusername:", self.username.text())
+                print("password:", self.password.text())
+                print("id:", self.user_id)
+                print("\nSuccessfully logged in!")
 
-            # self.toHome()
+                # to home
+                self.close()
+            else:
+                self.password.clear()
+                if self.password.text() == "":
+                    self.password.setStyleSheet(style.error)
+                self.password.textChanged.connect(self.pwdChanged)
+                self.password.setPlaceholderText(
+                    QtCore.QCoreApplication.translate(
+                        "LogIn", "⚠ Password is incorrect."
+                    )
+                )
         else:
             self.username.clear()
-            self.password.clear()
             if self.username.text() == "":
-                self.username.setStyleSheet(self.usnErrStyle)
+                self.username.setStyleSheet(style.error)
             self.username.textChanged.connect(self.usnChanged)
             self.username.setPlaceholderText(
                 QtCore.QCoreApplication.translate(
-                    "LogIn", "⚠ The username or password is incorrect."
+                    "LogIn", "⚠ The username does not exist."
                 )
             )
+            self.password.clear()
             if self.password.text() == "":
-                self.password.setStyleSheet(self.pwdErrStyle)
+                self.password.setStyleSheet(style.error)
             self.password.textChanged.connect(self.pwdChanged)
             self.password.setPlaceholderText(
-                QtCore.QCoreApplication.translate(
-                    "LogIn", "⚠ The username or password is incorrect."
-                )
+                QtCore.QCoreApplication.translate("LogIn", "⚠ Password is incorrect.")
             )
         event.accept()
 
     def signUp(self, event):
         self.sign_up = SignUp()
         self.sign_up.show()
-        self.hide()
+        self.close()
 
     def showPassword(self, event):
         if self.password.echoMode() == QtWidgets.QLineEdit.Normal:
@@ -101,13 +127,16 @@ class SignUp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("rsrc/ui/signup.ui", self)
+        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
         self.fontDB = QtGui.QFontDatabase()
         self.fontDB.addApplicationFont(":/Font/font/Better Grade/Better Grade.ttf")
         self.fontDB.addApplicationFont(
             ":/Font/font/Product Sans/Product Sans Regular.ttf"
         )
-        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.db = sqlite3.connect("rsrc/db/data.db")
+        self.curs = self.db.cursor()
+        self.setStyleSheet(style.default)
         self.ext_btn.mousePressEvent = self.login
         self.min_btn.mousePressEvent = self.minimize
         self.signup_btn.mousePressEvent = self.signUp
@@ -117,18 +146,6 @@ class SignUp(QtWidgets.QWidget):
         self.password.textChanged.connect(self.hidePassword)
         self.re_password.textChanged.connect(self.hideRePassword)
         self.cur_pos = QtCore.QPoint(1080, 620)
-        self.fnErrStyle = "#f_name { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.fnNormStyle = "#f_name { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
-        self.lnErrStyle = "#l_name { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.lnNormStyle = "#l_name { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
-        self.usnErrStyle = "#username { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.usnNormStyle = "#username { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
-        self.emlErrStyle = "#email { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.emlNormStyle = "#email { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
-        self.pwdErrStyle = "#password { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.pwdNormStyle = "#password { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
-        self.rpwdErrStyle = "#re_password { padding-left: 13px;  border-radius: 10px; background-color: rgb(255, 228, 184); color: red; font-size: 10px; }"
-        self.rpwdNormStyle = "#re_password { padding-left: 13px; border-radius: 10px; background-color: rgb(255, 228, 184); color: black; font-size: 14px; }"
 
     def mousePressEvent(self, event):
         self.cur_pos = event.globalPos()
@@ -146,89 +163,89 @@ class SignUp(QtWidgets.QWidget):
 
     def fnChanged(self, txt):
         if not txt:
-            self.f_name.setStyleSheet(self.fnErrStyle)
+            self.f_name.setStyleSheet(style.error)
         else:
-            self.f_name.setStyleSheet(self.fnNormStyle)
+            self.f_name.setStyleSheet(style.input)
 
     def lnChanged(self, txt):
         if not txt:
-            self.l_name.setStyleSheet(self.lnErrStyle)
+            self.l_name.setStyleSheet(style.error)
         else:
-            self.l_name.setStyleSheet(self.lnNormStyle)
+            self.l_name.setStyleSheet(style.input)
 
     def usnChanged(self, txt):
         if not txt:
-            self.username.setStyleSheet(self.usnErrStyle)
+            self.username.setStyleSheet(style.error)
         else:
-            self.username.setStyleSheet(self.usnNormStyle)
+            self.username.setStyleSheet(style.input)
 
     def emlChanged(self, txt):
         if not txt:
-            self.email.setStyleSheet(self.emlErrStyle)
+            self.email.setStyleSheet(style.error)
         else:
-            self.email.setStyleSheet(self.emlNormStyle)
+            self.email.setStyleSheet(style.input)
 
     def pwdChanged(self, txt):
         if not txt:
-            self.password.setStyleSheet(self.pwdErrStyle)
+            self.password.setStyleSheet(style.error)
         else:
-            self.password.setStyleSheet(self.pwdNormStyle)
+            self.password.setStyleSheet(style.input)
 
     def rpwdChanged(self, txt):
         if not txt:
-            self.re_password.setStyleSheet(self.rpwdErrStyle)
+            self.re_password.setStyleSheet(style.error)
         else:
-            self.re_password.setStyleSheet(self.rpwdNormStyle)
+            self.re_password.setStyleSheet(style.input)
 
     def signUp(self, event):
-        if self.f_name.text() == "":
-            self.f_name.setStyleSheet(self.fnErrStyle)
+        self.usn_query = "SELECT username FROM users"
+        self.curs.execute(self.usn_query)
+        self.users_db = self.curs.fetchall()
+        self.usn_db = [i[0] for i in self.users_db]
+        self.eml_query = "SELECT email FROM users"
+        self.curs.execute(self.eml_query)
+        self.emails_db = self.curs.fetchall()
+        self.eml_db = [i[0] for i in self.emails_db]
+        if not self.f_name.text():
+            self.f_name.setStyleSheet(style.error)
             self.f_name.textChanged.connect(self.fnChanged)
             self.f_name.setPlaceholderText(
-                QtCore.QCoreApplication.translate(
-                    "SignUp", "⚠ You must enter your first name."
-                )
+                QtCore.QCoreApplication.translate("SignUp", "⚠ First name is required.")
             )
-        if self.l_name.text() == "":
-            self.l_name.setStyleSheet(self.lnErrStyle)
+        if not self.l_name.text():
+            self.l_name.setStyleSheet(style.error)
             self.l_name.textChanged.connect(self.lnChanged)
             self.l_name.setPlaceholderText(
-                QtCore.QCoreApplication.translate(
-                    "SignUp", "⚠ You must enter your last name."
-                )
+                QtCore.QCoreApplication.translate("SignUp", "⚠ Last name is required.")
             )
-        if self.username.text() == "":
-            self.username.setStyleSheet(self.usnErrStyle)
+        if not self.username.text():
+            self.username.setStyleSheet(style.error)
             self.username.textChanged.connect(self.usnChanged)
             self.username.setPlaceholderText(
-                QtCore.QCoreApplication.translate(
-                    "SignUp", "⚠ You must enter your username."
-                )
+                QtCore.QCoreApplication.translate("SignUp", "⚠ Username is required.")
             )
-        if self.password.text() == "":
-            self.password.setStyleSheet(self.pwdErrStyle)
+        if not self.password.text():
+            self.password.setStyleSheet(style.error)
             self.password.textChanged.connect(self.pwdChanged)
             self.password.setPlaceholderText(
-                QtCore.QCoreApplication.translate(
-                    "SignUp", "⚠ You must enter your password."
-                )
+                QtCore.QCoreApplication.translate("SignUp", "⚠ Password is required.")
             )
-        if self.re_password.text() == "":
-            self.re_password.setStyleSheet(self.rpwdErrStyle)
+        if not self.re_password.text():
+            self.re_password.setStyleSheet(style.error)
             self.re_password.textChanged.connect(self.rpwdChanged)
-        if self.username.text() == "admin":  # if exists
+        if self.username.text() in self.usn_db:
             if self.username.text() == "":
-                self.username.setStyleSheet(self.usnErrStyle)
+                self.username.setStyleSheet(style.error)
             self.username.textChanged.connect(self.usnChanged)
             self.username.setPlaceholderText(
                 QtCore.QCoreApplication.translate(
-                    "SignUp", "⚠ Th username is already taken."
+                    "SignUp", "⚠ The username is already taken."
                 )
             )
             self.username.clear()
-        if self.email.text() == "admin@kmitl.ac.th":  # if exists
+        if self.email.text() in self.eml_db:
             if self.email.text() == "":
-                self.email.setStyleSheet(self.emlErrStyle)
+                self.email.setStyleSheet(style.error)
             self.email.textChanged.connect(self.emlChanged)
             self.email.setPlaceholderText(
                 QtCore.QCoreApplication.translate(
@@ -239,7 +256,7 @@ class SignUp(QtWidgets.QWidget):
         if len(self.password.text()) < 8:
             self.password.clear()
             self.re_password.clear()
-            self.password.setStyleSheet(self.pwdErrStyle)
+            self.password.setStyleSheet(style.error)
             self.password.textChanged.connect(self.pwdChanged)
             self.password.setPlaceholderText(
                 QtCore.QCoreApplication.translate(
@@ -249,37 +266,40 @@ class SignUp(QtWidgets.QWidget):
         if self.re_password.text() != self.password.text():
             self.password.clear()
             self.re_password.clear()
-            self.password.setStyleSheet(self.pwdErrStyle)
+            self.password.setStyleSheet(style.error)
             self.password.textChanged.connect(self.pwdChanged)
             self.password.setPlaceholderText(
-                QtCore.QCoreApplication.translate(
-                    "SignUp", "⚠ Password is not matched."
-                )
+                QtCore.QCoreApplication.translate("SignUp", "⚠ Password is unmatched.")
             )
-            self.re_password.setStyleSheet(self.rpwdErrStyle)
+            self.re_password.setStyleSheet(style.error)
             self.re_password.textChanged.connect(self.rpwdChanged)
             self.re_password.setPlaceholderText(
-                QtCore.QCoreApplication.translate(
-                    "SignUp", "⚠ Password is not matched."
-                )
+                QtCore.QCoreApplication.translate("SignUp", "⚠ Password is unmatched.")
             )
         if (
-            self.f_name.text() != ""
-            and self.l_name.text() != ""
-            and self.username.text() != ""
-            and self.password.text() != ""
-            and self.re_password.text() != ""
-            and self.username.text() != "admin"
-            and self.email.text() != "admin@kmitl.ac.th"
+            self.f_name.text()
+            and self.l_name.text()
+            and self.username.text()
+            and self.password.text()
+            and self.re_password.text()
+            and self.username.text() not in self.usn_db
+            and self.email.text() not in self.eml_db
             and len(self.password.text()) >= 8
             and self.re_password.text() == self.password.text()
-        ):  # if valid
-            print(self.f_name.text())
-            print(self.l_name.text())
-            print(self.username.text())
-            if self.email.text() != "":
-                print(self.email.text())
-            print(self.password.text())
+        ):
+            user_info = [
+                self.f_name.text(),
+                self.l_name.text(),
+                self.username.text(),
+                self.password.text(),
+                self.email.text() if self.email.text() else None,
+            ]
+            self.curs.execute(
+                "INSERT INTO users (fname, lname, username, password, email) VALUES (?,?,?,?,?)",
+                user_info,
+            )
+            self.db.commit()
+            self.db.close()
 
             self.log_in = LogIn()
             self.log_in.show()
